036751defbe2fcb721d705e118d0d8e2c0c7e704
Mathieu Hollebecq <mhollebecq@users.noreply.github.com>
2016-12-26 23:02:12 +0100

    Creating needed files

---
+++ ShapeFilesParser.Business/Models/PolygonZ.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShapeFilesParser.Business.Models
+{
+    public struct PolygonZ
+    {
+    }
+}
