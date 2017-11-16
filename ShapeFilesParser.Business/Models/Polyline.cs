94594b8c8f63b005168988d961a420f3599cb0ac
Mathieu Hollebecq <mhollebecq@users.noreply.github.com>
2017-11-16 16:03:28 +0100

    implemente polyline

---
+++ ShapeFilesParser.Business/Models/Polyline.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShapeFilesParser.Business.Models
+{
+    public struct Polyline
+    {
+        public double Xmin { get; set; }
+        public double Ymin { get; set; }
+        public double Xmax { get; set; }
+        public double Ymax { get; set; }
+
+        public int NumParts { get; set; }
+        public int NumPoints { get; set; }
+        public int[] Parts { get; set; }
+        public Point[] Points { get; set; }
+    }
+}
