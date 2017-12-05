a259247810f98a34c17a5ae2ed26965bcabe0e2c
Mathieu Hollebecq <mhollebecq@users.noreply.github.com>
2017-12-05 13:35:42 +0100

    change polygonZ

--- ShapeFilesParser.Business/Models/PolygonZ.cs
+++ ShapeFilesParser.Business/Models/PolygonZ.cs
@@ -8,33 +8,37 @@ namespace ShapeFilesParser.Business.Models
 {
     public struct PolygonZ
     {
-        private double[] marray;
-        private double mmax;
-        private double mmin;
-        private int numParts;
-        private int numPoints;
-        private int[] partsArray;
-        private Point[] pointsArray;
-        private double xmax;
-        private double xmin;
-        private double[] zarray;
-        private double zmax;
-        private double zmin;
+        public double Xmin { get; set; }
+        public double Ymin { get; set; }
+        public double Xmax { get; set; }
+        public double Ymax { get; set; }
+        public double Mmin { get; set; }
+        public double Zmin { get; set; }
+        public double Mmax { get; set; }
+        public double[] Marray { get; }
+        public double Zmax { get; set; }
+        public double[] Zarray { get; }
+        public int NumParts { get; set; }
+        public int NumPoints { get; set; }
+        public int[] Parts { get; set; }
+        public Point[] Points { get; set; }
 
-        public PolygonZ(double xmin, double xmax, int numParts, int numPoints, int[] partsArray, Point[] pointsArray, double zmin, double zmax, double[] zarray, double mmin, double mmax, double[] marray)
+        public PolygonZ(double xmin, double xmax, double ymin, double ymax, int numParts, int numPoints, int[] partsArray, Point[] pointsArray, double zmin, double zmax, double[] zarray, double mmin, double mmax, double[] marray)
         {
-            this.xmin = xmin;
-            this.xmax = xmax;
-            this.numParts = numParts;
-            this.numPoints = numPoints;
-            this.partsArray = partsArray;
-            this.pointsArray = pointsArray;
-            this.zmin = zmin;
-            this.zmax = zmax;
-            this.zarray = zarray;
-            this.mmin = mmin;
-            this.mmax = mmax;
-            this.marray = marray;
+            this.Xmin = xmin;
+            this.Xmax = xmax;
+            this.Ymin = ymin;
+            this.Ymax = ymax;
+            this.NumParts = numParts;
+            this.NumPoints = numPoints;
+            this.Parts = partsArray;
+            this.Points = pointsArray;
+            this.Zmin = zmin;
+            this.Zmax = zmax;
+            this.Zarray = zarray;
+            this.Mmin = mmin;
+            this.Mmax = mmax;
+            this.Marray = marray;
         }
     }
 }
