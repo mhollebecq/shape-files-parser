7c50542a27c1b21bf25019780932bcedc7ab07bc
Mathieu Hollebecq <mhollebecq@users.noreply.github.com>
2018-02-06 21:18:52 +0100

    forgotten files

---
+++ ShapeFilesParser.Linq/Query.cs
@@ -0,0 +1,69 @@
+﻿using ShapeFilesParser.Business;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+using System.Linq.Expressions;
+
+namespace ShapeFilesParser.Linq
+{
+    public class Query<T> : IQueryable<T>
+    {
+        private QueryProvider provider;
+        private Expression expression;
+
+        public Type ElementType
+        {
+            get
+            {
+                return typeof(T);
+            }
+        }
+
+        public Expression Expression
+        {
+            get { return this.expression; }
+        }
+
+        public IQueryProvider Provider
+        {
+            get { return this.provider; }
+        }
+
+        public Query(QueryProvider provider)
+        {
+            if (provider == null)
+                throw new ArgumentNullException("provider");
+            this.provider = provider;
+            this.expression = Expression.Constant(this);
+        }
+
+        public Query(QueryProvider provider, Expression expression)
+        {
+            if (provider == null)
+                throw new ArgumentNullException("provider");
+            if (expression == null)
+                throw new ArgumentNullException("expression");
+
+            if (!typeof(IQueryable<T>).IsAssignableFrom(expression.Type))
+            {
+                throw new ArgumentOutOfRangeException("expression");
+            }
+
+            this.provider = provider;
+            this.expression = expression;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return ((IEnumerable<T>)this.Provider.Execute(this.Expression)).GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return ((IEnumerable)this.Provider.Execute(this.Expression)).GetEnumerator();
+        }
+    }
+}
---
+++ ShapeFilesParser.Linq/QueryProvider.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShapeFilesParser.Linq
+{
+    public class QueryProvider : IQueryProvider
+    {
+        public IQueryable CreateQuery(Expression expression)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IQueryable<TElement> CreateQuery<TElement>(Expression expression)
+        {
+            throw new NotImplementedException();
+        }
+
+        public object Execute(Expression expression)
+        {
+            throw new NotImplementedException();
+        }
+
+        public TResult Execute<TResult>(Expression expression)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
